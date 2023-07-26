724995013569b7433ba19c3401d5e55b19444b95
vaidaoctav <octav.vaida@stud.ubbcluj.ro>
2023-07-26 14:37:41 +0300

    Added service and controller class for Student

---
+++ src/main/java/com/example/finalapplication/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.example.finalapplication.service;
+
+import com.example.finalapplication.domain.Student;
+import com.example.finalapplication.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    public Student findStudentById(Long id) {
+        return studentRepository.findById(id).orElseThrow(()->new RuntimeException("Student not found"));
+    }
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
